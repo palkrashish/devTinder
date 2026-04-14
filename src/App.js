3be97d916239702e3901aec619c1aaa90aa1a474
Ashish Kr Pal <palkrashish@gmail.com>
2026-04-15 00:06:36 +0530

    feat:- for checking route thing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express()
 
+app.get((req, res) => {
+    res.send("ha ha ha ha!")
+})
+
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
