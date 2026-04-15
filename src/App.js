214b6fa4e886d521e7d3b0e7e9b6a080408c6854
Ashish Kr Pal <palkrashish@gmail.com>
2026-04-16 00:04:27 +0530

    feat:- example of app.use thing and also some route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,23 @@ app.get("/main", (req, res) => {
     res.send("Hello Main!")
 })
 
+app.get('/fetch', (req, res) => {
+    res.send('Hell fetch!')
+})
+
+app.get('/Test', (req, res) => {
+    res.send('Hell Test!')
+})
+
+// This is handle all type of request
+app.use("/exampleuse", (req, res)=>{
+res.send('Example of use')
+})
+
+app.get('/ashish', (req, res) => {
+    res.send('Hell ashish!')
+})
+
 app.listen(3000, () => {
     console.log("Server started on port 3000")
 })
